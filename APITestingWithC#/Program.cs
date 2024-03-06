483e2d20131507c21e01622e6559c07e2782f691
Cvetelin Stoimenov <mishenata@abv.bg>
2024-03-06 22:50:08 +0200

    06.03.2024

--- APITestingWithC#/Program.cs
+++ APITestingWithC#/Program.cs
@@ -4,11 +4,25 @@ using System.Text.Json.Serialization;
 
 WetherForecast wetherForecast = new WetherForecast()
 {
-    Date = new DateTime(),
+    Date = DateTime.Now,
     TemperatureC = 32,
     Suumary = "New Random Test Summary"
 };
 
+// Serialize
+
 string wetherForecastJson = JsonSerializer.Serialize(wetherForecast);
 
 Console.WriteLine(wetherForecastJson);
+
+
+// Deserialize
+
+string jsonString = File.ReadAllText(Path.Combine(Environment.CurrentDirectory + "/../../../WetherForecast.json"));
+
+var weatherForecastsObject = JsonSerializer.Deserialize<List<WetherForecast>>(jsonString);
+
+foreach (var weatherForecast in weatherForecastsObject)
+{
+    Console.WriteLine($"Date: {weatherForecast.Date}, Temperature: {weatherForecast.TemperatureC}, Summary: {weatherForecast.Suumary}");
+}
