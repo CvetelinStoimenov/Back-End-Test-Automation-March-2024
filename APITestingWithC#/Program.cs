a60d9d39bc06a902b9be01c71aa15a62b1cb7d64
Cvetelin Stoimenov <mishenata@abv.bg>
2024-03-06 22:19:32 +0200

    Add project files.

---
+++ APITestingWithC#/Program.cs
@@ -0,0 +1,14 @@
+﻿using APITestingWithC_;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+WetherForecast wetherForecast = new WetherForecast()
+{
+    Date = new DateTime(),
+    TemperatureC = 32,
+    Suumary = "New Random Test Summary"
+};
+
+string wetherForecastJson = JsonSerializer.Serialize(wetherForecast);
+
+Console.WriteLine(wetherForecastJson);
---
+++ APITestingWithC#/WetherForecast.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APITestingWithC_
+{
+    internal class WetherForecast
+    {
+        public DateTime Date { get; set; }
+        public int TemperatureC { get; set; }
+        public string Suumary { get; set; }
+    }
+}
