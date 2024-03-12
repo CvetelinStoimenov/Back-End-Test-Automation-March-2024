46e6082aaaa52f8785ea8c92782bec1e0553d221
Cvetelin Stoimenov <mishenata@abv.bg>
2024-03-12 15:03:57 +0200

    12.03.2024

---
+++ GitHubAPITests_NUnit/Location.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace GitHubAPITests_NUnit
+{
+    public class Location
+    {
+        [JsonPropertyName("post code")]
+        public string PostCode { get; set; }
+
+        [JsonPropertyName("country")]
+        public string Country { get; set; }
+
+        [JsonPropertyName("country abbreviation")]
+        public string CountryAbbreviation { get; set; }
+
+        [JsonPropertyName("place")]
+        public List<Place> Place { get; set; }
+    }
+}
--- GitHubAPITests_NUnit/UnitTest1.cs
+++ GitHubAPITests_NUnit/UnitTest1.cs
@@ -1,6 +1,7 @@
 using NUnit.Framework.Internal;
 using RestSharp;
 using RestSharp.Authenticators;
+using System.Net;
 using System.Text.Json;
 
 namespace GitHubAPITests_NUnit
@@ -23,7 +24,7 @@ namespace GitHubAPITests_NUnit
         [Test]
         public void Test_GetAllIssuesFromARepo()
         {
-            var request = new RestRequest("/repos/testnakov/test-nakov-repo/issues", Method.Post);
+            var request = new RestRequest("/repos/testnakov/test-nakov-repo/issues", Method.Get);
             var response = client.Execute(request);
             var issues = JsonSerializer.Deserialize<List<Issue>>(response.Content);
 
@@ -35,6 +36,67 @@ namespace GitHubAPITests_NUnit
                 Assert.That(issue.number, Is.GreaterThan(0));
                 Assert.That(issue.title, Is.Not.Empty);
             }
+        }
+
+        private Issue CreateIssue(string title, string body)
+        {
+            var request = new RestRequest("/repos/testnakov/test-nakov-repo/issues");
+            request.AddBody(new { body, title});
+            var response = client.Execute(request, Method.Post);
+            var issue = JsonSerializer.Deserialize<Issue>(response.Content);
+            return issue;
+        }
+
+        [Test]
+        public void Test_CreateGitHubIssue()
+        {
+            string title = "This is a Demo Issue";
+            string body = "QA Back-End Automation Course February 2024, issue by Cvetelin Stoimenov";
+            var issue = CreateIssue(title, body);
+
+            Assert.That(issue.id, Is.GreaterThan(0));
+            Assert.That(issue.number, Is.GreaterThan(0));
+            Assert.That(issue.title, Is.Not.Empty);
+            Assert.That(issue.body, Is.EqualTo(body));
+        }
+
+        [Test]
+        public void Test_UpdateGitHubIssue()
+        {
+            var request = new RestRequest("repos/testnakov/test-nakov-repo/issues/6250");
+            request.AddJsonBody(new
+            {
+                title = "Changing the name of the issue that I created"
+            });
+
+            var response = client.Execute(request, Method.Patch);
+            var issue = JsonSerializer.Deserialize<Issue>(response.Content);
+
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+            Assert.That(issue.id, Is.GreaterThan(0), "Issue ID should be greater than 0.");
+            Assert.That(issue.number, Is.GreaterThan(0), "Issue number should be greater than 0.");
+            Assert.That(issue.title, Is.EqualTo("Changing the name of the issue that I created"));
+            Assert.That(response.Content, Is.Not.Empty, "The response content should not be empty.");
+        }
+
+
+        [TestCase("BG", "1000", "Sofia")]
+        [TestCase("BG", "5000", "Veliko Turnovo")]
+        [TestCase("CA", "M5S", "Toronto")]
+        [TestCase("GB", "B1", "Birmingham")]
+        [TestCase("DE", "01067", "Dresden")]
+        public void TestZippopotamus(string countryCode, string zipCode, string expectedPlace)
+        {
+            // Arrange
+            var restClient = new RestClient("https://api.zippopotam.us");
+            var httpRequest = new RestRequest(countryCode + "/" +  zipCode);
+
+            // Act
+            var httpResponse = restClient.Execute(httpRequest);
+            var location = new JsonDeserialize().Deserialize<Location>(httpResponse);
+
+            // Assert   
+            StringAssert.Contains(expectedPlace, location.Place[0].PlaceName);
         }
     }
 }
