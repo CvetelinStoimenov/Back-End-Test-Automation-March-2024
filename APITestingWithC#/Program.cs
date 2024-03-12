f314f439b7d31d3f74fe653e6ea3c2043fae5ff3
Cvetelin Stoimenov <mishenata@abv.bg>
2024-03-12 09:02:54 +0200

    12.03.2024

--- APITestingWithC#/Program.cs
+++ APITestingWithC#/Program.cs
@@ -1,5 +1,8 @@
 ﻿using APITestingWithC_;
+using RestSharp;
+using RestSharp.Authenticators;
 using System;
+using System.Text.Json;
 
 // Call the method to generate a WeatherForecast object
 WeatherForecast weatherForecast = SerializeDeserializeJSON.CreateRandomWeatherForecast();
@@ -21,4 +24,36 @@ SerializeDeserializeJSONNet.SerializeDeSerializeJSONNet(weatherForecast);
 // Deserialize the weather forecasts from the JSON file and print them using JSON.Net
 List<WeatherForecast> weatherForecastsJSONNet = SerializeDeserializeJSONNet.WeatherForecastProcessorJSONNet.DeserializeAndPrintWeatherForecastsJSONNet(filePathWeather);
 
-//var person = SerializeDeserializeJSONNet.DeSerializeAndPrintAnonymousTypeJSONNet(filePathPeople);
+var person = SerializeDeserializeJSONNet.DeSerializeAndPrintAnonymousTypeJSONNet(filePathPeople);
+
+var client = new RestClient("https://api.github.com");
+var request = new RestRequest("/repos/{user}/{repo}/issues/{id}", Method.Get);
+request.AddUrlSegment("user", "testnakov");
+request.AddUrlSegment("repo", "test-nakov-repo");
+request.AddUrlSegment("id", 1);
+var response = client.Execute(request);
+Console.WriteLine(response.StatusCode);
+Console.WriteLine(response.Content);
+
+var requestDeserialize = new RestRequest("/users/softuni/repos", Method.Get);
+var resp = client.Execute(requestDeserialize);
+
+var repos = JsonSerializer.Deserialize<List<Repo>>(resp.Content);
+
+foreach (var repo in repos)
+{
+    Console.WriteLine(repo.html_url);
+    Console.WriteLine(repo.id + " " + repo.full_name + Environment.NewLine);
+}
+
+
+var client = new RestClient(new RestClientOptions("https://api.github.com")
+{
+    Authenticator = new HttpBasicAuthenticator("CvetelinStoimenov", "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")
+});
+
+var requestPost = new RestRequest("/repos/testnakov/test-nakov-repo/issues", Method.Post);
+requestPost.AddHeader("Content-Type", "application/json");
+requestPost.AddJsonBody(new { title = "Test Stoimenov", body = "This text is posted from C# using ResSharp!" });
+var responsePost = client.Execute(requestPost);
+Console.WriteLine(responsePost.StatusCode);
