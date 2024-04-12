9e7b44474de5a2c376f0b485c3a4bfcda97dc063
Cvetelin Stoimenov <mishenata@abv.bg>
2024-04-12 13:08:18 +0300

    12.04.24

--- 11. Coding standards, code analysis/CodeAnalyzerDemo/Program.cs
+++ 11. Coding standards, code analysis/CodeAnalyzerDemo/Program.cs
@@ -1,16 +1,14 @@
-﻿using System;
-
-class Program
+﻿class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hi!"); 
+        Console.WriteLine("Hi!");
     }
 
     private string MakeGreeting(string name)
     {
         if (name == null) // No braces and spacing issues
             throw new ArgumentNullException("name");
-        return "Hello, " + name; 
+        return "Hello, " + name;
     }
 }
