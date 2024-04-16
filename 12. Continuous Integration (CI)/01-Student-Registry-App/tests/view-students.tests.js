aba32e6c4a27104ec0cd62d85d409d74cb7a1d13
Cvetelin Stoimenov <mishenata@abv.bg>
2024-04-16 20:10:00 +0300

    add

--- 12. Continuous Integration (CI)/01-Student-Registry-App/tests/view-students.tests.js
+++ 12. Continuous Integration (CI)/01-Student-Registry-App/tests/view-students.tests.js
@@ -3,14 +3,14 @@ const fetch = require('node-fetch');
 
 suite('View Students page', function() {
   test('Page title', async function() {
-    let res = await fetch("http://localhost:8888/students");
+    let res = await fetch("http://localhost:8081/students");
     let body = await res.text();
     assert.ok(body.includes("<h1>Registered Students</h1>"));
   });
   
   test('Students list', async function() {
-    let res = await fetch("http://localhost:8888/students");
+    let res = await fetch("http://localhost:8081/students");
     let body = await res.text();
-    assert.ok(body.includes("<ul><li>Steve (steve@gmail.com)</li><li>Tina (tina@yahoo.com)</li></ul>"));
+    assert.ok(body.includes("<ul><li>Marry (marry@gmail.com)</li><li>Steve (steve@yahoo.com)</li><li>Teddy (teddy@mail.ru)</li></ul>"));
   });
 });
